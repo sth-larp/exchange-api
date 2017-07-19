f6ca03eea1b2f4fe73e6f586fbcee1ec5e71fb66
Andrey Krivtsov <andy@alice.local>
2017-07-19 18:57:37 +0300

    Change create MBX script

--- src/create-mailboxes.ps1
+++ src/create-mailboxes.ps1
@@ -1,18 +1,82 @@
-param (
+﻿param (
     [Parameter(Mandatory = $true)] [string] $InputFile
 )
 
-#"Test String: $InputFile" | Write-Host
-#Get-Location
+#$fileName = "params.json"
 
-#Get-Content -Path $InputFile | Write-Host
+if(-not (Test-Path $InputFile)){
+    Write-Error "Can't read file $InputFile!"
+    exit
+}
 
-$ret = @{
-    results = @()
+$login = "alice\andy"
+$password = "de@sEx2@!7"
+$OU = "OU=Users,OU=Org,DC=alice,DC=local"
+
+$usersList  = Get-Content -Path $InputFile | Out-String | ConvertFrom-Json
+
+$secPassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$creds = new-object -typename System.Management.Automation.PSCredential -argumentlist $login, $secPassword
+
+
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://ex01.alice.local/PowerShell/ -Authentication Kerberos -Credential $creds -WarningAction SilentlyContinue
+
+
+$ret = @{ status = ""; message =""; users = @()  }
+
+if(!$session){ 
+    $ret.status = "global-error";
+    $ret.message = "Can't open session to Exchange server!"
+    $ret | ConvertTo-Json | Write-Host
+    exit
 } 
 
-$ret.results +=  @{ name = "name1"; result = "result1" }
-$ret.results +=  @{ name = "name1"; result = "result1" }
+Import-PSSession $session -AllowClobber -WarningAction SilentlyContinue -DisableNameChecking | Out-Null
 
+$ret.status = "ok"
+
+$usersList.users | %{
+
+    $user = $_
+
+    $pass = $user.password | ConvertTo-SecureString -AsPlainText -Force
+    $upn = $user.login + "@alice.digital"
+    $sma = "user" + $user.id
+
+    try { 
+        
+        $mailbox = Get-Mailbox $sma -ErrorAction SilentlyContinue
+    
+        if($mailbox -eq $null){
+            #Create Mailbox
+
+            $mailbox = New-Mailbox -Name $sma -Password $pass -UserPrincipalName $upn -DisplayName $user.fullName `
+                -FirstName $user.firstName -LastName $user.lastName -Alias $user.login -OrganizationalUnit $OU -SamAccountName $sma -WarningAction SilentlyContinue
+    
+            Set-Mailbox $sma -ExtensionCustomAttribute1 $user.id -ExtensionCustomAttribute2 $user.password -HiddenFromAddressListsEnabled:$true -WarningAction SilentlyContinue | Out-Null
+            
+            $ret.users += @{ id = $user.id; login = $user.login; result = "created" }
+        }else{
+            #update Mailbox
+
+            if( ($mailbox.Alias -ne $user.login) -or ($mailbox.DisplayName -ne $user.fullName) -or ($mailbox.ExtensionCustomAttribute2 -ne $user.password) ) {
+
+                $mailbox | Set-Mailbox -Password $pass -UserPrincipalName $upn -DisplayName $user.fullName -ExtensionCustomAttribute2 $user.password`
+                    -Alias $user.login -WarningAction SilentlyContinue | Out-Null
+
+                get-user $sma | Set-User -FirstName $user.firstName -LastName $user.lastName -WarningAction SilentlyContinue | out-null
+
+                $ret.users += @{ id = $user.id; login = $user.login; result = "updated" }
+            }else{
+                $ret.users += @{ id = $user.id; login = $user.login; result = "not changed" }
+            }
+        }
+    } 
+    catch {
+        $ret.users += @{ id = $user.id; login = $user.login; result = "error" }
+        $ret.status = "error"
+    }
+}
 
 $ret | ConvertTo-Json | Write-Host
+
